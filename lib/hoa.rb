f794f0b98a2a3cfce63133deac314b5cb93acfbb
Amanda Rolon <abrolon87@gmail.com>
2019-11-12 17:44:05 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -9,9 +9,7 @@ def add_character(show, name)
   [:chipmunks] << "Dave"
   [:third_earthers] << "Snarf"
   [:jetsons] << "Astro"
-  BASE_HOA[:chipmunks]
-  BASE_HOA[:third_earthers]
-  BASE_HOA[:jetsons]
+  add_character
 end
 #daily_weather[:temperature][2] = 74
 #daily_weather[:precipitation] << 1.01 #=> [0.0, 0.01, 0.03, 1.01]
