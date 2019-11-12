383932cbcc951421797ed24ad1e9c699a70e3304
Amanda Rolon <abrolon87@gmail.com>
2019-11-12 17:47:27 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -9,5 +9,4 @@ def add_character(show, name)
   BASE_HOA[show] << name
   BASE_HOA[show]
 end
-#daily_weather[:temperature][2] = 74
-#daily_weather[:precipitation] << 1.01 #=> [0.0, 0.01, 0.03, 1.01]
+
